4e44de56fe4a240426fd95e5292d2ad248a897af
blackatz <ksenya.noskova.98@inbox.ru>
2022-11-21 19:23:03 +0300

    new

---
+++ 41/Program.cs
@@ -0,0 +1,75 @@
+﻿//пользователь ввел с клавиатуры Н чисел.
+//посчитайте, сколько цифр больше 0 ввел пользователь
+
+{
+Console.Clear();
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.WriteLine("Подсчет положительных чисел");
+Console.WriteLine("");
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("числа перечисляются через запятую");
+Console.ResetColor();
+
+int[] arrayNumbers = InputArrayNumbers(); // ввод массива чисел с разбором на части
+
+Console.WriteLine(PozitiveNumbers(arrayNumbers));
+
+
+Console.WriteLine("");
+Console.ForegroundColor = ConsoleColor.Cyan;
+Console.Write("для завершения задачи нажмите любую клавишу...");
+Console.ResetColor();
+Console.ReadKey(true);
+}
+
+static int[] InputArrayNumbers()
+    {
+        Console.WriteLine("");
+
+        NewInput:
+
+        Console.Write("Введите числа: ");
+        string str  = Console.ReadLine()!;
+        string[] str_ = str.Split(','); // разбить на элементы
+        int[] rezult = new int[str_.Length];
+        for (int i = 0; i < str_.Length; i++)
+        {
+            string tempStr = str_[i].Trim(); //удаление боковых пробелов
+
+            if(int.TryParse(tempStr, out int tempInt)) // если элемент число
+            {
+                rezult[i] = tempInt; //запись элемента
+            }
+            else //если элемент не число
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"err: некорректный ввод {i + 1}-го элемента!");
+                Console.ResetColor();
+
+                goto NewInput; //сначала
+            }
+            }
+            return rezult;
+        }
+
+        int PozitiveNumbers(int[] arrayNumbers)
+        {   
+            int pozitive = 0;
+            int i = 0;
+            int[] rezult = new int[arrayNumbers.Length];
+            
+            while (i < arrayNumbers.Length)
+            {
+                if (arrayNumbers[i] > 0)
+                {
+                    pozitive++;
+                    i++; }
+
+                else
+                {
+                    i++;
+                }
+            }
+
+            return pozitive;
+        }
---
+++ 43/Program.cs
@@ -0,0 +1,39 @@
+﻿//Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+Console.WriteLine("Программа для поиска точки пересечения прямых");
+Console.WriteLine("Введите k1: ");
+
+double k1 = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите b1: ");
+
+double b1 = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите k2: ");
+
+double k2 = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите b2: ");
+
+double b2 = double.Parse(Console.ReadLine()!);
+
+double Find(double k1, double b1, double k2, double b2)
+    {   double x = 0;
+        double y = 0;
+        if (k1 == k2)
+    {
+        Console.WriteLine("Прямые параллельны");
+    }
+        else 
+        {
+        x = (b2-b1)/(k1-k2);
+        y = k2 * x + b2;
+        }
+        Console.WriteLine("Координаты точки пересечения: " + x + ";" + y);
+        return x;
+    }
+
+Find(k1, b1, k2, b2);
